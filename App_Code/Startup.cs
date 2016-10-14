f757b93bddcd00a4308d3f1096221c53efd98c94
Daniel034 <daniel.hall03@live.com>
2016-10-14 14:56:01 -0500

    Add files via upload

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hall_Resume.Startup))]
+namespace Hall_Resume
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
